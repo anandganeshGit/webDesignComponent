ebc806a9634cf9ee3fd60eb584d805feff5a642a
HuskyAnand <balasubramanian.a@husky.neu.edu>
2018-03-30 02:26:18 -0400

    Class Assgn

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import{Hero} from './hero';
+
+export const HEROES:Hero[] =[
+    {id:0001,name:'Dhoni',age:12},
+    {id:0002,name:'sachin',age:23},
+    {id:0003,name:'rohit',age:21},
+    {id:0004,name:'virat',age:25},
+    {id:0005,name:'nehra',age:null}
+];
